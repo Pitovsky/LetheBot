464e8a3e6ae9efcd9a405e473db08dc17a616478
pitovsky <pet.pohta@gmail.com>
2025-01-25 15:26:54 +0100

    sh setup (not working)

--- setup.sh
+++ setup.sh
@@ -4,12 +4,54 @@ echo "Setting up your own self-hosted Lethe."
 echo "Please open the following page in your browser and login with your TG phone number:"
 echo "https://my.telegram.org/apps"
 echo "Copy the details from the page (the data is not passed anywhere but your self-hosted app):"
-read -p "App api_id:" APP_API_ID
-read -p "App api_hash": APP_API_HASH
-read -p "Your phone number:" PHONE_NUMBER
+echo -n "App api_id:"
+read -sr TG_API_ID
+echo -en "\nApp api_hash:"
+read -sr TG_API_HASH
+echo -en "\nYour phone number:"
+read -r  PHONE_NUMBER
 
-echo
-echo "$SESSION_ID" > keys.env
+cat << EOT > keys.env
+TG_API_ID=$TG_API_ID
+TG_API_HASH=$TG_API_HASH
+TG_BOT_TOKEN=TBD
+TG_SESSION_STR=TBD
+EOT
 
-doctl sls deploy lethebot -env keys.env
-doctl invoke lethebot
+echo "Deploying your setup function (may take a few minutes)..."
+doctl sls deploy . --include lethebot/setup --env keys.env
+echo "Calling setup function to initiate session, be ready to receive SMS..."
+ACTIVATION=$(doctl sls fn invoke lethebot/setup --param "phone:$PHONE_NUMBER" --no-wait)
+RUNNING=1
+while [ $RUNNING ]
+do
+
+done
+IFS="|" read -r TG_PHONE_HASH TG_BOT_TOKEN <<< "$SETUP_DATA"
+echo "TODO: hash: $TG_PHONE_HASH"
+echo "Enter the code:"
+read -sr TG_PHONE_CODE
+
+echo "Calling setup function to log in..."
+SETUP_DATA=$(doctl sls fn invoke lethebot/setup \
+  --param "phone:$PHONE_NUMBER" \
+  --param "hash:$TG_PHONE_HASH" \
+  --param "code:$TG_PHONE_CODE")
+IFS="|" read -r TG_SESSION_STR TG_BOT_TOKEN <<< "$SETUP_DATA"
+
+cat <<EOT > keys.env
+TG_API_ID=$TG_API_ID
+TG_API_HASH=$TG_API_HASH
+TG_BOT_TOKEN=$TG_BOT_TOKEN
+TG_SESSION_STR=$TG_SESSION_STR
+EOT
+
+exit 0
+
+echo "Deploying the main function"
+doctl sls deploy lethebot/tg_webhook -env keys.env
+WEBHOOK_URL=$(doctl sls fn get lethebot/tg_webhook --url)
+curl -X POST "https://api.telegram.org/bot$TG_BOT_TOKEN/setWebhook?url=$WEBHOOK_URL"
+
+echo "Complete!"
+echo "All the further interaction happens through the bot, so you can safely remove this repo now."
