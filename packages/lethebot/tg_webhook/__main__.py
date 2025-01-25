18c03268aa1be5ebc8cb8ee3c4013fb2d49e0e14
Alex Kulikov <alexeyqu@gmail.com>
2025-01-25 14:07:10 +0000

    feat: automated bot creation

--- packages/lethebot/tg_webhook/__main__.py
+++ packages/lethebot/tg_webhook/__main__.py
@@ -3,6 +3,7 @@ import logging
 import os
 
 from bot import get_bot
+from tg_client import TgClient
 
 logger = logging.getLogger(__name__)
 
@@ -21,5 +22,11 @@ if __name__ == '__main__':
     session = os.environ.get('TG_SESSION_STR')
     api_id = os.environ.get('TG_API_ID')
     api_hash = os.environ.get('TG_API_HASH')
-    bot = get_bot(os.environ.get('TG_BOT_TOKEN'))
-    bot.run_polling()
+    if os.environ.get('TG_BOT_TOKEN'):
+        # we already have a bot
+        bot = get_bot(os.environ.get('TG_BOT_TOKEN'))
+        bot.run_polling()
+    else:
+        # need to create a bot
+        tg_client = TgClient(session, api_id, api_hash)
+        asyncio.run(tg_client.create_new_bot())
